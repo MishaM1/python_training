5b0f84e94dc8659da080825a3e2826ace6828245
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-05-20 22:38:49 +0300

    Домашнее задание №2 - рефакторинг параметров

--- second_test.py
+++ second_test.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
 
+
 def is_alert_present(wd):
     try:
         wd.switch_to_alert().text
@@ -10,25 +11,33 @@ def is_alert_present(wd):
     except:
         return False
 
+
 class second_test(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
-    
+
     def test_second_test(self):
-        success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_css_selector("html").click()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        self.open_homepage(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.new_group(wd)
+        self.filling_group_form(wd)
+        self.submit_group(wd)
+        self.group_page_open(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def group_page_open(self, wd):
         wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
+
+    def submit_group(self, wd):
+        wd.find_element_by_name("submit").click()
+
+    def filling_group_form(self, wd):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("test group2")
@@ -38,13 +47,29 @@ class second_test(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("test")
-        wd.find_element_by_name("submit").click()
+
+    def new_group(self, wd):
+        wd.find_element_by_name("new").click()
+
+    def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    def login(self, wd, username="admin", password="secret"):
+        wd.find_element_by_css_selector("html").click()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_homepage(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
