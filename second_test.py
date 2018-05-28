5038ccbc7f0f8a6beae8d8786127db6dfccd2b40
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-05-28 10:35:58 +0300

    self.wd - вынесено

--- second_test.py
+++ second_test.py
@@ -18,33 +18,35 @@ class second_test(unittest.TestCase):
         self.wd.implicitly_wait(60)
 
     def test_second_test(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.new_group(wd)
-        self.filling_group_form(wd, Group(name="test group2", header="test", footer="test_r"))
-        self.submit_group(wd)
-        self.group_page_open(wd)
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.new_group()
+        self.filling_group_form(Group(name="test group2", header="test", footer="test_r"))
+        self.submit_group()
+        self.group_page_open()
+        self.logout()
 
     def test_empty_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.new_group(wd)
-        self.filling_group_form(wd, Group(name="", header="", footer=""))
-        self.submit_group(wd)
-        self.group_page_open(wd)
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.new_group()
+        self.filling_group_form( Group(name="", header="", footer=""))
+        self.submit_group()
+        self.group_page_open()
+        self.logout()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def group_page_open(self, wd):
+    def group_page_open(self):
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def submit_group(self, wd):
+    def submit_group(self):
+        wd = self.wd
         wd.find_element_by_name("submit").click()
 
-    def filling_group_form(self, wd, group):
+    def filling_group_form(self, group):
+        wd = self.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -55,15 +57,18 @@ class second_test(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
 
-    def new_group(self, wd):
-        self.open_group_page(wd)
+    def new_group(self):
+        wd = self.wd
+        self.open_group_page()
         wd.find_element_by_name("new").click()
 
-    def open_group_page(self, wd):
+    def open_group_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd, username, password):
-        self.open_homepage(wd)
+    def login(self, username, password):
+        wd = self.wd
+        self.open_homepage()
         wd.find_element_by_css_selector("html").click()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -73,7 +78,8 @@ class second_test(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def open_homepage(self, wd):
+    def open_homepage(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
