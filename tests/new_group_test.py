78f6d32f3f40472ef3b51f7d7956cddb260cd33a
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-06-02 12:55:18 +0300

    Домашнее задание #5 - добавлены классы помощники для групп и контактов

--- tests/new_group_test.py
+++ tests/new_group_test.py
@@ -12,10 +12,10 @@ def app(request):
 
 def test_add_new_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test group2", header="test", footer="test_r"))
+    app.group.create(Group(name="test group2", header="test", footer="test_r"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group( Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
--- tests/new_user_test.py
+++ tests/new_user_test.py
@@ -14,14 +14,14 @@ def app(request):
 def test_add_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="Misha", last_name="New", address="New York",
-            home_phone="555555555555", mobile_phone="555555555555564", work_phone="45646465465465456", fax="none",
-            first_email="testye@gmail.com", second_email="sadasdas@gmail.com", third_email="sadasda@mail.ru"))
+    app.contact.create(Contact(first_name="Misha", last_name="New", address="New York",
+                               home_phone="555555555555", mobile_phone="555555555555564", work_phone="45646465465465456", fax="none",
+                               first_email="testye@gmail.com", second_email="sadasdas@gmail.com", third_email="sadasda@mail.ru"))
     app.session.logout()
 
 def test_add_empty_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="", last_name="", address="", home_phone="", mobile_phone="",
-             work_phone="", fax="", first_email="", second_email="", third_email=""))
+    app.contact.create(Contact(first_name="", last_name="", address="", home_phone="", mobile_phone="",
+                               work_phone="", fax="", first_email="", second_email="", third_email=""))
     app.session.logout()
