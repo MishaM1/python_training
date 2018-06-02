926a0ddc1b7a946ecd7e8249ba50c119e7a8b40d
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-06-02 19:23:48 +0300

    Выполнено д/з №8: Добавлена "интеллектуальность" фикстуры и функций login и logout, откритие домашней страницы убрано из теста по созданию юзера

--- tests/new_user_test.py
+++ tests/new_user_test.py
@@ -3,13 +3,11 @@ from python_training.model.contacts import Contact
 
 
 def test_add_contact(app):
-    app.open_home_page()
     app.contact.create(Contact(first_name="Misha", last_name="New", address="New York",
                                home_phone="555555555555", mobile_phone="555555555555564", work_phone="45646465465465456", fax="none",
                                first_email="testye@gmail.com", second_email="sadasdas@gmail.com", third_email="sadasda@mail.ru"))
 
 
 def test_add_empty_contact(app):
-    app.open_home_page()
     app.contact.create(Contact(first_name="", last_name="", address="", home_phone="", mobile_phone="",
                                work_phone="", fax="", first_email="", second_email="", third_email=""))
