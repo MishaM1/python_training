00f1b37492733685d0532acf44ebe8c6b425062f
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-06-03 13:44:03 +0300

    Выполнено задание №10 и добавленна оптимизация переходов между страницами

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,6 @@ class Application:
 
     def __init__ (self):
         self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -24,7 +23,8 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//form[@id='LoginForm']/input[3]")) > 0):
+            wd.get("http://localhost/addressbook/")
 
     def return_to_home_page(self):
         wd = self.wd
