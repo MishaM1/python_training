32e2aa971e4acab82d89e2bc0acab4f32d5e7e19
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-06-15 22:23:26 +0300

    Испрвлено домашнее задание - Добавлены проверкы в тесты модификации контактов

--- tests/test_delete_user.py
+++ tests/test_delete_user.py
@@ -2,5 +2,5 @@ from python_training.model.contacts import Contact
 
 def test_delete_first_user(app):
     if app.contact.count_contacts() == 0:
-        app.contact.create(Contact(first_name="Created group for deletion"))
+        app.contact.create(Contact(first_name="Created contact for deletion"))
     app.contact.delete_first_user()
--- tests/test_modify_user.py
+++ tests/test_modify_user.py
@@ -2,12 +2,18 @@ from python_training.model.contacts import Contact
 
 
 def test_modify_contact_name(app):
+    if app.contact.count_contacts() == 0:
+        app.contact.create(Contact(first_name="Created contact for modify"))
     app.contact.modify_first_contact(Contact(first_name="New name"))
 
 
 def test_modify_contact_home_phone(app):
+    if app.contact.count_contacts() == 0:
+        app.contact.create(Contact(first_name="Created contact for modify"))
     app.contact.modify_first_contact(Contact(home_phone="88888888888888888888"))
 
 
 def test_modify_contact_first_email(app):
+    if app.contact.count_contacts() == 0:
+        app.contact.create(Contact(first_name="Created contact for modify"))
     app.contact.modify_first_contact(Contact(first_email="new_mail@gmail.com"))
