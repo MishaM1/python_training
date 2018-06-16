0b171a88e29a42483a2947f0a945277983a0408c
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-06-16 14:49:04 +0300

    Комит с ошибкой сравнения контактов

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -4,12 +4,14 @@ from python_training.model.contacts import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(first_name="Misha", last_name="New", address="New York",
+    contact = Contact(first_name="Misha", last_name="New", address="New York",
                                home_phone="555555555555", mobile_phone="555555555555564", work_phone="45646465465465456", fax="none",
-                               first_email="testye@gmail.com", second_email="sadasdas@gmail.com", third_email="sadasda@mail.ru"))
+                               first_email="testye@gmail.com", second_email="sadasdas@gmail.com", third_email="sadasda@mail.ru")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
-
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 def test_add_empty_contact(app):
     old_contacts = app.contact.get_contact_list()
