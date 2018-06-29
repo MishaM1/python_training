34d2369c4ce4cd45596aff823fe939b511ca5a60
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-06-29 12:55:43 +0300

    Реализованы опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from python_training.fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__ (self, browser="firefox"):
+    def __init__ (self, browser,base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox(capabilities={"marionette": False})
         elif browser == "chrome":
@@ -18,6 +18,7 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -30,7 +31,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//form[@id='LoginForm']/input[3]")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def return_to_home_page(self):
         wd = self.wd
