66f2ce703b56c23d3429025997d3133d91d44216
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-06-30 13:39:46 +0300

    Добавлен генератор файла для групп и контактов с использованием getOpt

--- generator/contact.py
+++ generator/contact.py
@@ -3,6 +3,23 @@ import random
 import string
 import json
 import os.path
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
 
 
 def random_string_contacts(prefix, maxlen):
@@ -22,10 +39,10 @@ testdata = [Contact(firstname="", lastname="", address="",
             secondaryphone=random_string_contacts("secondaryphone", 10), fax=random_string_contacts("fax", 10),
             first_email=random_string_contacts("first_email", 10), second_email=random_string_contacts("second_email", 10),
             third_email=random_string_contacts("third_email", 10))
-    for i in range(5)
+    for i in range(n)
 ]
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/contacts.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
