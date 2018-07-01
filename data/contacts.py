5a9960f5bbc369a12689cd9ef9cfefc2d3bfc1c8
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-07-01 21:47:35 +0300

    Реализован генератор тестовых данных для групп и контактов, сохраняющие созданные данные в файл в формате JSON.

---
+++ data/contacts.py
@@ -0,0 +1,15 @@
+from python_training.model.contacts import Contact
+
+
+testdata = [
+    Contact(firstname="11", lastname="11", address="11",
+                      homephone="11", workphone="11",
+                      mobilephone="11", secondaryphone="11",
+                      fax="11", first_email="11",
+                      second_email="11", third_email="11"),
+    Contact(firstname="22", lastname="22", address="22",
+                      homephone="22", workphone="22",
+                      mobilephone="22", secondaryphone="22",
+                      fax="22", first_email="22",
+                      second_email="22", third_email="22")
+]
