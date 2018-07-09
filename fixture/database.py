a5e0c48932e67513a496fae25b6f6364d0802098
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-07-09 21:21:27 +0300

    Реалищовано сравнение БД и ЮИ для групп и контактов

--- fixture/database.py
+++ fixture/database.py
@@ -1,5 +1,6 @@
 import pymysql
 from python_training.model.group import Group
+from python_training.model.contacts import Contact
 
 class DbFixture:
     def __init__(self, host, name, user, password):
@@ -17,6 +18,20 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3 from addressbook")
+            for row in cursor:
+                (id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address,
+                 homephone=home, mobilephone=mobile, workphone=work, secondaryphone=phone2,
+                                    first_email=email, second_email=email2, third_email=email3))
         finally:
             cursor.close()
         return list
--- tests/test_db_matches_ui.py
+++ tests/test_db_matches_ui.py
@@ -1,5 +1,5 @@
 from python_training.model.group import Group
-
+from python_training.model.contacts import Contact
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()
@@ -7,3 +7,14 @@ def test_group_list(app, db):
         return Group(id.group.id, name=group.name.strip())
     db_list = db.get_group_list()
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname, lastname=contact.lastname,
+                       address=contact.address, homephone=contact.homephone, mobilephone=contact.mobilephone,
+                       workphone=contact.workphone, secondaryphone=contact.secondaryphone, first_email=contact.first_email, second_email=contact.second_email,
+                       third_email=contact.third_email)
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
