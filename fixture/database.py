ef4de5d1871f7059fef55dd77e0b9ea0842bfd59
Mykhaylo Malyshev <mmalyshev76@gmail.com>
2018-07-14 20:48:00 +0300

    реализованы прверки с БД и добавлены отключаемые проверки для групп и контактов. Добавлен отсев удалленных записей

--- fixture/database.py
+++ fixture/database.py
@@ -28,7 +28,7 @@ class DbFixture:
         contact_list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3, fax from addressbook")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3, fax from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3, fax) = row
                 contact_list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address,
